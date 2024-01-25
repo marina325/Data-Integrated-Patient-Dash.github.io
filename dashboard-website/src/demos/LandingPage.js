708d84f4dc50e7ddaa74d8de2a271169d4600212
Melissa2002 <mosherof@ucsd.edu>
2024-01-25 13:42:43 -0800

    Update LandingPage.js

--- dashboard-website/src/demos/LandingPage.js
+++ dashboard-website/src/demos/LandingPage.js
@@ -2,8 +2,7 @@ import { React, useRef }  from "react";
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import Hero from "components/hero/TwoColumnWithPrimaryBackground.js";
 import NewTeam from "components/features/NewTeam"
-
-import Diaries from "components/features/Diaries";
+import Diary from "components/features/Diaries/Diary.jsx"
 
 export default () => {
   const newTeamRef = useRef(null)
@@ -11,7 +10,7 @@ export default () => {
   return (
     <AnimationRevealPage>
       <Hero newTeamRef={newTeamRef} dashboardRef={dashboardRef}/>
-      {/* <Diary></Diary> */}
+      <Diary/>
     </AnimationRevealPage>
   );
 }
